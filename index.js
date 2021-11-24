57e6d749d8d85cec81fe3405d49b37a7d4f7cd2d
Vuong Do <vuongvgc@gmail.com>
2021-11-24 22:11:45 +0700

    Feature -  oauth20 - login

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./config/keys");
 const app = express();
 
-// Router handle
-app.get("/", (req, res) => {
-  res.send({
-    status: "success",
-    message: "Hi there, you ara logined",
-  });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken) => {
+      console.log("access token", accessToken);
+    }
+  )
+);
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
