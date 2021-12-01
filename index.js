341314d40308798b35b8b3c9af03dd4eb307a3fd
Vuong Do <vuongvgc@gmail.com>
2021-12-01 22:16:00 +0700

    Feature-  index - connect mongodb use mongoose

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const key = require("./config/keys");
 const app = express();
 
 require("./services/passport");
 require("./routers/authRouter")(app);
-
+mongoose.connect(key.mongoURI);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
